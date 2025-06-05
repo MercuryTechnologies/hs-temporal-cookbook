bf7e44b4ee1cd9f7e20691bf035ded06f2c9bb40
Matt Olson <matto@mercury.com>
2025-06-05 15:43:57 -0700

    add retryable route to toy service

--- temporal101/HelloServer.hs
+++ temporal101/HelloServer.hs
@@ -1,10 +1,23 @@
+import Data.IORef
+import Network.HTTP.Types.Status
 import Web.Scotty
 
 main :: IO ()
-main = scotty 9001 $ do
-  get "/hello/:name" $ do
-    name <- pathParam "name"
-    text $ "¡Hola, " <> name <> "!"
-  get "/goodbye/:name" $ do
-    name <- pathParam "name"
-    text $ "¡Adios, " <> name <> "!"
+main = do
+  attempts <- newIORef 0
+  scotty 9001 $ do
+    get "/hello/:name" $ do
+      name <- pathParam "name"
+      text $ "¡Hola, " <> name <> "!"
+    get "/goodbye/:name" $ do
+      name <- pathParam "name"
+      text $ "¡Adios, " <> name <> "!"
+    get "/sometimesBusy" $ do
+      liftIO $ modifyIORef' attempts (+1)
+      attempt <- liftIO $ readIORef attempts
+      if attempt < 3 then failureResponse else successResponse
+      where
+        failureResponse = do
+          status status429
+          text "please call back later"
+        successResponse = text "thank you, drive through"
