2424e460539f4f86c74ac60af8425c4858542b13
Matt Olson <matto@mercury.com>
2025-06-19 10:38:48 -0700

    copy ex1, update service, separate workflow

---
+++ temporal102/ex2/Workflow.hs
@@ -0,0 +1,99 @@
+module Workflow where
+
+import Control.Exception (throw)
+import Control.Monad (forever)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Logger (defaultOutput, runStdoutLoggingT)
+import Control.Monad.Trans.Reader (runReaderT)
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text (Text)
+import Data.Text qualified as T
+import Data.Text.Encoding (decodeUtf8)
+import Data.UUID qualified as UUID
+import Data.UUID.V4 qualified as UUID.V4
+import DiscoverInstances (discoverInstances)
+import GHC.Generics (Generic)
+import Network.HTTP.Simple (httpBS, getResponseBody, getResponseStatusCode, parseRequest)
+import RequireCallStack (RequireCallStack, provideCallStack)
+import System.IO (stdout)
+import Temporal.Activity (Activity)
+import Temporal.Client (mkWorkflowClientConfig, workflowClient)
+import Temporal.Client qualified as Client
+import Temporal.Core.Client (connectClient, defaultClientConfig)
+import Temporal.Duration (seconds)
+import Temporal.Exception (ApplicationFailure (..))
+import Temporal.Runtime (TelemetryOptions (..), initializeRuntime)
+import Temporal.TH (WorkflowFn, ActivityFn)
+import Temporal.TH qualified
+import Temporal.Worker (Worker, WorkerConfig, startWorker)
+import Temporal.Worker qualified as Worker
+import Temporal.Workflow (Workflow, WorkflowId (..))
+import Temporal.Workflow qualified as Workflow
+import UnliftIO.Concurrent (threadDelay)
+import UnliftIO.Exception (bracket)
+
+tshow :: Show a => a -> Text
+tshow = T.pack . show
+
+data TranslateTermInput = TranslateTermInput
+  { term :: Text
+  , lang :: Text
+  }
+  deriving stock (Generic, Show)
+  deriving anyclass (ToJSON, FromJSON)
+
+translateActivity :: TranslateTermInput -> Activity () Text
+translateActivity TranslateTermInput{..} = do
+  req <- parseRequest $ "http://localhost:9001/translate/" ++ T.unpack term ++ "/" ++ T.unpack lang
+  resp <- httpBS req
+  let body = decodeUtf8 $ getResponseBody resp
+  let status = getResponseStatusCode resp
+  case status of
+    200 -> pure body
+    _ -> reportTranslationError status body
+  where
+    reportTranslationError err body = throw
+      ApplicationFailure
+        { type' = "TranslationError"
+        , message = "Status " <> tshow err <> ": " <> tshow body
+        , nonRetryable = True
+        , details = []
+        , stack = ""
+        , nextRetryDelay = Nothing
+        }
+
+-- Register the activity with Temporal's code generation
+Temporal.TH.registerActivity 'translateActivity
+
+-- | Workflow configuration options for activities
+activityOptions :: Workflow.StartActivityOptions
+activityOptions =
+  Workflow.defaultStartActivityOptions (Workflow.StartToClose $ seconds 30)
+
+data TranslationInput = TranslationInput
+  { name :: Text
+  , languageCode :: Text
+  } 
+  deriving stock (Generic, Show)
+  deriving anyclass (FromJSON, ToJSON)
+
+data TranslationOutput = TranslationOutput
+  { helloMessage :: Text
+  , goodbyeMessage :: Text
+  }
+  deriving stock (Generic, Show)
+  deriving anyclass (FromJSON, ToJSON)
+
+sayHelloGoodbyeWorkflow :: TranslationInput -> Workflow TranslationOutput
+sayHelloGoodbyeWorkflow input = provideCallStack $ do
+  hello <- Workflow.executeActivity TranslateActivity activityOptions $ TranslateTermInput "hello" input.languageCode
+  let helloMsg = hello <> ", " <> input.name
+  
+  Workflow.sleep (seconds 10)
+  
+  goodbye <- Workflow.executeActivity TranslateActivity activityOptions $ TranslateTermInput "goodbye" input.languageCode
+  let goodbyeMsg = goodbye <> ", " <> input.name
+  
+  pure $ TranslationOutput helloMsg goodbyeMsg
+
+Temporal.TH.registerWorkflow 'sayHelloGoodbyeWorkflow
