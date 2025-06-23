fb0462873b8f5db8ca7f33415eaac9c9a72b6ada
jkachmar <j@mercury.com>
2025-06-23 15:28:22 -0400

    feat(temporal102): init age-estimation sample
    so it turns out that the API used as part of this sample doesn't support
    TLS1.2+EMS (Extended Main Secret), which 'http-client-tls' is set up to
    require by default.
    
    there are a few ways we could have handled this, but I think it actually
    makes for a nice example for how dependency injection works for the
    activity's 'MonadReader' instance.

---
+++ temporal102/samples/age-estimation/Shared.hs
@@ -0,0 +1,54 @@
+module Shared where
+
+-- NOTE: Required to import workflow instances that this worker will handle.
+import Workflow ()
+
+import Control.Monad.Logger (defaultOutput)
+import Control.Monad.IO.Class (MonadIO)
+import Data.Default (def)
+import DiscoverInstances (discoverInstances)
+import Network.Connection (settingClientSupported)
+import Network.HTTP.Client qualified as HTTP.Client
+import Network.HTTP.Client.TLS (mkManagerSettings, newTlsManagerWith)
+import qualified Network.TLS as TLS
+import RequireCallStack (RequireCallStack, provideCallStack)
+import System.IO (stdout)
+import Temporal.TH (WorkflowFn, ActivityFn)
+import Temporal.TH qualified
+import Temporal.Worker (WorkerConfig)
+import Temporal.Worker qualified as Worker
+import Temporal.Workflow qualified as Workflow
+
+mkWorkerConfig :: HTTP.Client.Manager -> WorkerConfig HTTP.Client.Manager
+mkWorkerConfig manager = provideCallStack $ Worker.configure manager definitions settings
+  where
+    definitions :: RequireCallStack => Worker.Definitions HTTP.Client.Manager
+    definitions = Temporal.TH.discoverDefinitions $$(discoverInstances) $$(discoverInstances)
+    settings = do
+      Worker.setNamespace namespace
+      Worker.setTaskQueue taskQueue
+      Worker.setLogger (defaultOutput stdout)
+
+namespace :: Workflow.Namespace
+namespace = "default"
+
+taskQueue :: Workflow.TaskQueue
+taskQueue = "test"
+
+-- | The third-party service used to implement this example supports TLS, but
+-- does /not/ support TLS1.2+EMS (Extended Main Secret).
+--
+-- The 'http-client-tls' connection manager has 'TLS.RequireEMS' set by
+-- default, which causes requests to fail.
+--
+-- Fortunately this makes for a useful example of how we can use the 'Activity'
+-- environment to as a form of dependency injection.
+newRelaxedEMSManager :: MonadIO m => m HTTP.Client.Manager
+newRelaxedEMSManager = do
+  let tlsSettings = def {
+        settingClientSupported = def {
+          TLS.supportedExtendedMainSecret = TLS.AllowEMS
+        }
+      }
+  newTlsManagerWith $ mkManagerSettings tlsSettings Nothing
+
