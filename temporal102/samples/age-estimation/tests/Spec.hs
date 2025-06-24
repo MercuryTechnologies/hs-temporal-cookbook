22cec8a060dc097541d8283e77ac75a345c230a2
jkachmar <j@mercury.com>
2025-06-24 13:08:44 -0400

    feat(temporal102): age-estimation mock workflow

--- temporal102/samples/age-estimation/tests/Spec.hs
+++ temporal102/samples/age-estimation/tests/Spec.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import ActivitySpec qualified
+import MockWorkflowSpec qualified
 import WorkflowSpec qualified
 
 import Test.Hspec
@@ -8,4 +9,5 @@ import Test.Hspec
 main :: IO ()
 main = hspec do
   ActivitySpec.spec
+  MockWorkflowSpec.spec
   WorkflowSpec.spec
